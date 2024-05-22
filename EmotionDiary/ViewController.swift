b68605fa76d21711c441a06e7883079befa38e87
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-05-22 18:41:05 +0900

    UserDefault, 리셋버튼 추가
    - App이 background에 들어갔을 때 UserDefault에 Data 저장
    - 구조체 타입을 저장하기 위한
    - Encoding, Decoding 메서드 추가
    - 랜덤으로 이미지를 가져오는 리셋버튼 추가

--- EmotionDiary/ViewController.swift
+++ EmotionDiary/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-fileprivate struct Emotion {
+fileprivate struct Emotion: Codable {
     let emotion: String
     let imageName: String
     var count = 0
@@ -21,23 +21,63 @@ class ViewController: UIViewController {
     @IBOutlet var emotionButtons: [UIButton]!
     @IBOutlet var emotionLabels: [UILabel]!
     
-    private var emotions: [Emotion] = [
-        Emotion(emotion: "행복해", imageName: "slime1"),
-        Emotion(emotion: "사랑해", imageName: "slime3"),
-        Emotion(emotion: "좋아해", imageName: "slime2"),
-        Emotion(emotion: "당황해", imageName: "slime7"),
-        Emotion(emotion: "속상해", imageName: "slime8"),
-        Emotion(emotion: "우울해", imageName: "slime9"),
-        Emotion(emotion: "지루해", imageName: "slime4"),
-        Emotion(emotion: "졸려", imageName: "slime6"),
-        Emotion(emotion: "무덤덤해", imageName: "slime5")
-    ]
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var resetLabel: UILabel!
+    
+    private var emotions: [Emotion] = []
+    
+    private let emotionKey = "emotions"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loadUserDefault()
         titleBarConfig()
         buttonConfig()
+        resetButtonConfig()
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(appMovedToBackground),
+                                       name: UIApplication.willResignActiveNotification,
+                                       object: nil)
+    }
+    
+    @objc func appMovedToBackground() {
+        saveUserDefault()
+    }
+    
+    private func saveUserDefault() {
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(emotions) {
+            UserDefaults.standard.set(encoded, forKey: emotionKey)
+            print("UserDefault 저장 완료")
+        } else {
+            print("UserDefault 저장 실패")
+        }
+    }
+    
+    private func loadUserDefault() {
+        if let data = UserDefaults.standard.object(forKey: emotionKey) as? Data {
+            let decoder = JSONDecoder()
+            if let emotionList = try? decoder.decode([Emotion].self, from: data) {
+                self.emotions = emotionList
+                print("UserDefault 불러오기 완료")
+                return
+            }
+        }
+        self.emotions = [
+            Emotion(emotion: "행복해", imageName: "slime1"),
+            Emotion(emotion: "사랑해", imageName: "slime3"),
+            Emotion(emotion: "좋아해", imageName: "slime2"),
+            Emotion(emotion: "당황해", imageName: "slime7"),
+            Emotion(emotion: "속상해", imageName: "slime8"),
+            Emotion(emotion: "우울해", imageName: "slime9"),
+            Emotion(emotion: "지루해", imageName: "slime4"),
+            Emotion(emotion: "졸려", imageName: "slime6"),
+            Emotion(emotion: "무덤덤해", imageName: "slime5")
+        ]
+        print("UserDefault 불러오기 실패")
     }
     
     private func titleBarConfig() {
@@ -60,10 +100,33 @@ class ViewController: UIViewController {
         }
     }
     
+    private func resetButtonConfig() {
+        var image: UIImage?
+        
+        if let randomEmotion = emotions.randomElement() {
+            let imageName = randomEmotion.imageName
+            image = UIImage(named: imageName)?.withRenderingMode(.alwaysOriginal)
+        } else {
+            image = UIImage.slime1.withRenderingMode(.alwaysOriginal)
+        }
+        self.resetButton.setImage(image, for: .normal)
+        
+        self.resetLabel.text = "리셋"
+        self.resetLabel.font = .boldSystemFont(ofSize: 15)
+        self.resetLabel.textAlignment = .center
+    }
+    
     @IBAction func buttonTapped(_ sender: UIButton) {
         let tag = sender.tag
-        self.emotions[tag].count += 1
-        self.emotionLabels[tag].text = emotions[tag].emotion + "\(emotions[tag].count)"
+        if tag == 9 {
+            for i in 0..<9 {
+                self.emotions[i].count = 0
+                self.emotionLabels[i].text = emotions[i].emotion + "\(emotions[i].count)"
+            }
+        } else {
+            self.emotions[tag].count += 1
+            self.emotionLabels[tag].text = emotions[tag].emotion + "\(emotions[tag].count)"
+        }
     }
     
 }
